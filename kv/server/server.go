2a068e49834e5b666dc2cf737aec40ac98bda6c0
HUSTERGS <1148690954@qq.com>
2020-08-02 01:56:57 +0800

    :white_check_mark: lab1 finished
    完成lab1

--- kv/server/server.go
+++ kv/server/server.go
@@ -38,22 +38,75 @@ func NewServer(storage storage.Storage) *Server {
 // Raw API.
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return nil, err
+	}
+	// 此处注意出错的时候不应该直接返回nil，而是返回一个标示为notfound的Response，并且err似乎始终nil?
+	val, err := reader.GetCF(req.Cf, req.Key)
+	if err != nil || val == nil{
+		return &kvrpcpb.RawGetResponse{NotFound: true}, nil
+	}
+
+	response := &kvrpcpb.RawGetResponse{
+		Value:    val,
+		NotFound: false,
+	}
+	return response, nil
 }
 
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+
+	if err := server.storage.Write(req.Context, []storage.Modify{
+		{
+			storage.Put{
+				Key:   req.Key,
+				Value: req.Value,
+				Cf:    req.Cf,
+			}}}); err != nil {
+		// FIXME: 测试中似乎不怎么关心第一个返回值，暂时先设置为nil
+		return nil, nil
+	}
+	return &kvrpcpb.RawPutResponse{}, nil
 }
 
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+	// 与RawPut基本相同
+	if err := server.storage.Write(req.Context, []storage.Modify{
+		{
+			storage.Delete{
+				Key:   req.Key,
+				Cf:    req.Cf,
+			}}}); err != nil {
+		// FIXME: 测试中似乎不怎么关心第一个返回值，暂时先设置为nil
+		return nil, nil
+	}
+	return &kvrpcpb.RawDeleteResponse{}, nil
 }
 
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+	respKvPairs := []*kvrpcpb.KvPair{}
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		return nil, err
+	}
+	iterator := reader.IterCF(req.Cf)
+	iterator.Seek(req.StartKey)
+	for i := uint32(0); i < req.GetLimit(); i++ {
+		if !iterator.Valid() {
+			break
+		}
+		val, _ := iterator.Item().Value()
+		respKvPairs = append(respKvPairs, &kvrpcpb.KvPair{
+			Key:                  iterator.Item().Key(),
+			Value:                val,
+		})
+		iterator.Next()
+	}
+	return &kvrpcpb.RawScanResponse{Kvs: respKvPairs}, nil
 }
 
 // Raft commands (tinykv <-> tinykv)
--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,8 +1,10 @@
 package standalone_storage
 
 import (
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -10,29 +12,89 @@ import (
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	engine *engine_util.Engines
+	conf * config.Config
+}
+
+type StandAloneStorageReader struct {
+	// FIXME: 暂时先不管raft
+	kvTxn  * badger.Txn
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+
+	return &StandAloneStorage{conf: conf}
 }
 
 func (s *StandAloneStorage) Start() error {
 	// Your Code Here (1).
+	// 由于close方法需要保证两个都有，所以暂时先初始化一个
+	s.engine = engine_util.NewEngines(engine_util.CreateDB("kv", s.conf),
+		engine_util.CreateDB("raft", s.conf), "kv", "raft")
 	return nil
 }
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
+	// 不知道比不必要，关于Log的使用
+	if err := s.engine.Close(); err != nil {
+		//log.Fatal(err)
+		return err
+	}
 	return nil
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return nil, nil
+	// 暂时不管ctx
+	return &StandAloneStorageReader{
+		kvTxn: s.engine.Kv.NewTransaction(false),
+	}, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
+	// 暂时不用管这个ctx
+	txn := s.engine.Kv.NewTransaction(true)
+	for _, m := range batch {
+		// 此处参照modify.go
+		switch m.Data.(type) {
+		case storage.Put:
+			if err := txn.Set(engine_util.KeyWithCF(m.Cf(), m.Key()), m.Value()); err != nil{
+				return err
+			}
+		case storage.Delete:
+			if err :=txn.Delete(engine_util.KeyWithCF(m.Cf(), m.Key())); err != nil{
+				return err
+			}
+		}
+		if err := txn.Commit(); err != nil {
+			return err
+		}
+	}
+
 	return nil
+}
+
+func (reader * StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+	val, err := engine_util.GetCFFromTxn(reader.kvTxn, cf, key)
+	if err != nil {
+		// 这个地方需要手动写一个判断，由于测试`TestRawDelete1`中要求没有找到的时候返回nil, nil
+		if err == badger.ErrKeyNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return val, nil
+}
+
+
+func (reader * StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
+	return engine_util.NewCFIterator(cf, reader.kvTxn)
+}
+
+func (reader * StandAloneStorageReader) Close () {
+	reader.kvTxn.Discard()
 }
